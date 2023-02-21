aed64db6548b42b00faf37228d1d0b871e45faae
=TamiselvanAvatar <=tamilselvanunofficialavatar@gmail.com>
2023-02-22 00:58:47 +0530

    ext-1

--- content.js
+++ content.js
@@ -4,6 +4,8 @@ btnAll = document.getElementsByTagName('button')
 //Store Connect button details alone
 btnConnect = []
 
+countConnect = 0 
+
 for(let i in btnAll){
 	
 	//Get Connect button from All Button Array
@@ -19,9 +21,22 @@ interval =  setInterval (()=>{
 	btn.style.backgroundColor = "green";
 	if(btnConnect.length === 0){
 		console.log("Connect request done")
+		chrome.runtime.sendMessage({type: "notification", options : { status : "Done" }});
 		clearInterval(interval)
 	}
+	countConnect = countConnect +  1
+	chrome.runtime.sendMessage({type: "notification", options: { 
+		status: "Connect Request",
+		count: countConnect
+	}});
 },5000)
+
+chrome.runtime.onMessage.addListener((msg , request, sender) => {
+	console.log("MSG",msg)
+	if(msg.type === "notification"){
+		clearInterval(interval)
+	}
+})
 
 //const event = new MouseEvent("click", {
  //   view: window,
--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-document.getElementById("connect").addEventListener("click", e =>{
+ibtnConnect = document.getElementById("connect");
+ibtnTimer = document.getElementById("timer-count");
+close = false
+ibtnConnect.addEventListener("click", e =>{
 	chrome.tabs.query({active:true,currentWindow:true},tabs=>{
 		chrome.scripting.executeScript({target:{tabId:tabs[0].id}, files:['content.js']})
 	})
+	
+	if(close){
+		chrome.runtime.sendMessage({type: "notification", options : { status : "Stop" }})
+	}
+})
+
+
+chrome.runtime.onMessage.addListener((msg , request, sender) => {
+	console.log("BTN",ibtnConnect)
+	console.log("TIM",ibtnTimer)
+	console.log(msg)
+	if(msg.type === "notification"){
+		const text = msg.options.status === "Done" ? "Connection Done" : "Stop Connection";
+		ibtnConnect.innerText = text
+		// ibtnConnect.disabled = true
+		ibtnTimer.innerText = msg.options.count
+		close = true
+	}
 })
