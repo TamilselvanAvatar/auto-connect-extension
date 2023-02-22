a05c40d13b7219765256b51fbba6c284fa8c4a43
=TamiselvanAvatar <=tamilselvanunofficialavatar@gmail.com>
2023-02-23 00:48:19 +0530

    ext-3

--- content.js
+++ content.js
@@ -1,51 +1,68 @@
 //Get All button details of page
-btnAll = document.getElementsByTagName('button')
+btnAll = document.getElementsByTagName("button");
 
 //Store Connect button details alone
-btnConnect = []
+btnConnect = [];
 
-countConnect = 0 
+countConnect = 0;
 
-for(let i in btnAll){
-	
-	//Get Connect button from All Button Array
-    if(btnAll[i]['ariaLabel'] && btnAll[i]['ariaLabel'].match(/^Invite.*connect$/)){
-        btnConnect = [...btnConnect, btnAll[i]]
+totalRequest = 0;
+
+interval = 0;
+
+clickEvent = new MouseEvent("click", {
+  view: window,
+  bubbles: true,
+  cancelable: true,
+});
+
+chrome.runtime.onMessage.addListener((msg, request, sender) => {
+  btnAll = document.getElementsByTagName("button");
+  for (let i in btnAll) {
+    //Get Connect button from All Button Array
+    if (
+      btnAll[i]["ariaLabel"] &&
+      btnAll[i]["ariaLabel"].match(/^Invite.*connect$/)
+    ) {
+      btnConnect = [...btnConnect, btnAll[i]];
     }
-	
-}
+  }
 
-// Set Interval to initiate the connect request 
-interval =  setInterval (()=>{
-	btn = btnConnect.shift();
-	btn.style.backgroundColor = "green";
-	if(btnConnect.length === 0){
-		console.log("Connect request done")
-		chrome.runtime.sendMessage({type: "notification", options : { status : "Done" }});
-		clearInterval(interval)
-	}
-	countConnect = countConnect +  1
-	chrome.runtime.sendMessage({type: "notification", options: { 
-		status: "Connect Request",
-		count: countConnect
-	}});
-},5000)
+  totalRequest = btnConnect.length;
 
-chrome.runtime.onMessage.addListener((msg , request, sender) => {
-	console.log("MSG",msg)
-	if(msg.type === "notification"){
-		clearInterval(interval)
-	}
-})
-
-//const event = new MouseEvent("click", {
- //   view: window,
-  //  bubbles: true,
- //   cancelable: true,
-  //});
-//document.getElementById("input").dispatchEvent(event)
-
-
-
-
-
+  console.log("MSG from Content", msg);
+  if (msg?.event) {
+    interval = setInterval(() => {
+      btn = btnConnect.shift();
+	  countConnect = countConnect + 1;
+    //   btn.dispatchEvent(clickEvent)
+      btn.style.backgroundColor = "green";
+      if (btnConnect.length === 0) {
+        console.log("Connect request done");
+        chrome.runtime.sendMessage({
+          type: "notification",
+          options: {
+            status: "Done",
+            count: countConnect,
+            total: totalRequest,
+          },
+        });
+        clearInterval(interval);
+      } else {
+        chrome.runtime.sendMessage({
+          type: "notification",
+          options: {
+            status: "Connect Request",
+            count: countConnect,
+            clear: interval,
+            total: totalRequest,
+          },
+        });
+      }
+    }, 5000);
+  }
+  if (msg?.status === "Stop") {
+    clearInterval(msg.interval);
+  }
+  console.log({ btnAll, btnConnect, countConnect, interval });
+});
--- index.js
+++ index.js
@@ -1,25 +1,37 @@
 ibtnConnect = document.getElementById("connect");
 ibtnTimer = document.getElementById("timer-count");
+clear = 0 
 close = false
-ibtnConnect.addEventListener("click", e =>{
-	chrome.tabs.query({active:true,currentWindow:true},tabs=>{
-		chrome.scripting.executeScript({target:{tabId:tabs[0].id}, files:['content.js']})
-	})
-	
-	if(close){
-		chrome.runtime.sendMessage({type: "notification", options : { status : "Stop" }})
+ibtnConnect.addEventListener("click", e =>{	
+	if(close && clear){
+		chrome.tabs.query({active:true,currentWindow:true}, function(tab) {
+			chrome.tabs.sendMessage(tab[0].id, {status : "Stop" , interval : clear});
+		}); 
+		ibtnConnect.style.display = "none"
+		close = false
+	}
+	else{
+
+		ibtnConnect.style.backgroundColor = "#ea4646cc"
+		ibtnConnect.style.color = "white"
+		ibtnConnect.innerText = "Connecting..."
+		chrome.tabs.query({active:true,currentWindow:true}, function(tab) {
+			chrome.tabs.sendMessage(tab[0].id, {event:true});
+		});
 	}
 })
 
 
 chrome.runtime.onMessage.addListener((msg , request, sender) => {
-	console.log("BTN",ibtnConnect)
-	console.log("TIM",ibtnTimer)
-	console.log(msg)
 	if(msg.type === "notification"){
 		const text = msg.options.status === "Done" ? "Connection Done" : "Stop Connection";
+		clear = msg.options.clear
 		ibtnConnect.innerText = text
-		// ibtnConnect.disabled = true
+		if(msg.options.count === msg.options.total){
+			console.log(msg, "disabled")
+			ibtnConnect.disabled = true
+			ibtnConnect.style.backgroundColor = "##58df44cc"
+		} 
 		ibtnTimer.innerText = msg.options.count
 		close = true
 	}
